3b990baa3b5a30dbe378144ed97f0717f910f5f8
Godwin <goodgodwin@hotmail.com>
2017-04-09 12:09:33 -0700

    2017 updates

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,19 +5,19 @@ basedir = File.join(Dir.pwd, 'app', 'assets', 'stylesheets')
 # precompile all scss files for each bumbleberry file
 precompile = Array.new
 ((Bumbleberry::settings['precompile'] || {})[Rails.env] || {}).each do |k,v|
-	v.each do |version|
-		precompile << "#{k}-#{version}"
-	end
+  v.each do |version|
+    precompile << "#{k}-#{version}"
+  end
 end
 
 precompile << '*' if precompile.empty?
 
 precompile.each do |p|
-	Rails.application.config.assets.precompile << File.join('web-fonts', "#{p}.css")
+  Rails.application.config.assets.precompile << File.join('web-fonts', "#{p}.css")
 end
 
 Bumbleberry::settings['stylesheets'].each do | path |
-	precompile.each do |p|
-		Rails.application.config.assets.precompile << File.join(path, "#{p}.css")
-	end
+  precompile.each do |p|
+    Rails.application.config.assets.precompile << File.join(path, "#{p}.css")
+  end
 end
